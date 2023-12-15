0eda61cf47e88fd007af3c01a6441f0b0bb60469
Mayank Shukla <117716383+mayankshukla-20@users.noreply.github.com>
2023-12-15 16:58:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 const makeChange = (c) => {
   // your name here
+	let change = {
+		q: 0,
+		d: 0,
+		n: 0,
+		p: 0
+	}
+	while(c>=0){
+		if(c>=25){
+			change.q += 1;
+			c = c - 25;
+		}
+		else if(c>=10){
+			change.d += 1;
+			c = c - 10;
+		}
+		else if(c>=5){
+			change.n += 1;
+			c = c - 5;
+		}
+		else if(c>=1){
+			change.p += 1;
+			c = c - 1;
+		}
+	}
+	return change;
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
+// const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
